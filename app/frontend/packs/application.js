4ef3262ae61af1121645d382bebe36a026f44574
Andrew Mason <andrew.mason@n2pub.com>
2019-02-01 01:05:50 -0500

    ad eslint disable

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -5,4 +5,6 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+
+// eslint-disable-next-line no-console
 console.log('hello');
